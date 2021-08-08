487dea3318a9e34d7d2b1ccd9d32d9569a982c07
Peterturn <55192090+Peterturn@users.noreply.github.com>
2021-08-08 09:43:28 +0200

    worked on updating the menu functions to get the values from the sliders as well as updating the feature search object

--- src/app/main-page-folder/menu/menu.component.ts
+++ src/app/main-page-folder/menu/menu.component.ts
@@ -30,23 +30,11 @@ export class MenuComponent implements OnInit {
   currentYear = new Date().getFullYear();
 
   //tables or arrays
-  marquesArr: any = ["volkswagen"];
-  modeleArr: any = ["mustange", "peter"];
-  carb = ["Carburant", "Électrique", "Essence", "Diesel"]
+  marquesArr: any = ["BRAND"];
+  modeleArr: any = ["MODEL"];
+  carb = ["ELECTRIC", "GASOLINE", "DIESEL", "HYBRID"]
 
 
-  //bools
-
-  menuDisplayed = false;
-
-  boolTbl: { [key: string]: boolean } = {
-    marqueBool: false,
-    modeleBool: false,
-    carbBool: false,
-    slideBool: false,
-    kmBool: false,
-    prixBool: false,
-  }
   //strings
   marqueString = "BRAND";
   modeleString = "MODEL";
@@ -55,92 +43,102 @@ export class MenuComponent implements OnInit {
   kilometrage = "KILOMETERS";
   prix = "PRICE";
 
-  //filter view
-  searchObject = {
-    marqueStringa: "",
-    modeleString: "",
-    carbString: "",
-    slideNombre: this.slideNombre,
-    kilometrage: this.kilometrage,
-    prix: this.prix,
-  }
-
-  //Working
   getMarque(carObjsArr: {}) {
     let carArr = Object.entries(carObjsArr);
     let carObjArr: any = carArr[0][1];
 
     //populates carListing array with current carAd objects
     this.carListingArr = carObjArr;
-    console.log(this.carListingArr);
 
     let brandArr = carObjArr.map((carAd: Car) => carAd.brand)
     const noDuplicates = [...new Set(brandArr)];
+
+    // Brand/Marque Array is populated with results after duplicates are removed
     return this.marquesArr = noDuplicates;
   }
 
 
   changeStringMQ(val: string): void {
     this.marqueString = val;
+    this.searchObj.marqueStringa = val;
     this.modeleString = "MODEL";
     // filter the objects and create array that has only the same brand name
     const tempArr = this.carListingArr.filter((e: any) => e.brand.toLocaleUpperCase() == this.marqueString.toLocaleUpperCase());
     // returns an array of only the model names
     const tempArr2 = tempArr.map((e: any) => e.model);
-    console.log(tempArr2);
 
-    //erases duplicates
+    //remove duplicates by creating a unique Set.
     const tempArr3 = [...new Set(tempArr2)];
-    console.log(tempArr3);
 
+    //Populates Car Model array with results based on brand selected.
     this.modeleArr = tempArr3;
+
+
     if (this.marqueString == "BRAND") {
       this.modeleString = "MODEL";
-      this.boolTbl.modeleBool = false;
     }
-    if (this.marqueString !== "BRAND")
-      this.boolTbl.marqueBool = !this.boolTbl.marqueBool;
+
   }
 
-  //working
+
   changeStringMD(val: string): void {
     this.modeleString = val;
-    this.boolTbl.modeleBool = !this.boolTbl.modeleBool;
+    this.searchObj.modeleString = val;
   }
+
   changeStringCarb(val: string) {
     this.carbString = val;
-    this.searchObject.carbString = val;
-    this.boolTbl.carbBool = !this.boolTbl.carbBool;
+    this.searchObj.carbString = val;
+    this.searchObj.carbString = val;
   }
-  getPrice($event: any): void {
-    console.log($event.target);
 
-    this.searchObject.prix = $event.target.value;
+  getYear($event: any): void {
+    this.searchObj.slideNombre = $event.target.value;
   }
-  //understand this
-  closeOtherMenues(state: string): void {
-    for (const a in this.boolTbl) {
-      if (a !== state) { this.boolTbl[a] = false; }
-    }
-    this.boolTbl[state] = !this.boolTbl[state];
+
+  getKilometers($event: any): void {
+    this.searchObj.kilometrage = $event.target.value;
   }
+
+  getPrice($event: any): void {
+    this.searchObj.prix = $event.target.value;
+  }
+
+
 
   myBigValider(): void {
-    //the big reset
-    this.marqueString = "Marque";
-    this.modeleString = "Modèle";
-    this.carbString = "Carburant";
-    this.slideNombre = "l'Annee";
-    this.kilometrage = "kilometrage";
-    this.prix = "prix";
-    for (const a in this.boolTbl) {
-      this.boolTbl[a] = false;
-    }
+    console.log(this.searchObj);
+
+    //button reset
+    this.marqueString = "BRAND";
+    this.modeleString = "MODEL";
+    this.carbString = "FUEL TYPE";
+    this.slideNombre = "YEAR";
+    this.kilometrage = "KILOMETERS";
+    this.prix = "PRICE";
+
+  }
+
+  //filter view
+  searchObj = {
+    marqueStringa: null,
+    modeleString: null,
+    carbString: null,
+    slideNombre: "1920",
+    kilometrage: "300000",
+    prix: "300000"
   }
 }
 
 
+//study this CODE snippet
 
+// closeOtherMenues(state: string): void {
+//   for(const a in this.boolTbl) {
+//   if (a !== state) { this.boolTbl[a] = false; }
+// }
+// this.boolTbl[state] = !this.boolTbl[state];
+//   }
 
 
 
